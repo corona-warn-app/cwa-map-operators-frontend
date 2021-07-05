db2751d3e37d6ebeb32c7c697d0747ca7928631d
Dirk Reske <dirk.reske@t-systems.com>
2021-07-05 13:04:18 +0200

    Translated error messages, upload template included

--- src/app/shared/pipes/error-message.pipe.ts
+++ src/app/shared/pipes/error-message.pipe.ts
@@ -5,17 +5,35 @@ import {Pipe, PipeTransform} from '@angular/core';
 })
 export class ErrorMessagePipe implements PipeTransform {
 
+  private readonly validationPattern = /'(\w+)' failed for '(\w+)'/;
+  private readonly invalidTestKind = /invalid testkind: (\w+)/;
+
   private translations: { [id: string]: string } = {
     "no valid testkinds found": "Keine gültigen Testarten gefunden",
     "invalid appointment type": "Ungültiger Termintyp",
     "invalid enter date": "Ungültiges Eintrittsdatum",
     "invalid leave date": "Ungültiges Austrittsdatum",
     "invalid image data": "Ungültige Bildinformationen",
-    "invalid testkind:": "Ungültige Testart:"
+    "invalid testkind:": "Ungültige Testart:",
+    "validation_required": "ist erforderlich",
+    "validation_max": "ist zu lang"
   }
 
   transform(value: string, ...args: unknown[]): unknown {
-    return this.translations[value] || value;
+    if (this.translations[value]) {
+      return this.translations[value];
+    } else {
+      let matcher = value.match(this.validationPattern);
+      if (matcher != null && matcher.length == 3) {
+        return matcher[1] + " " + this.transform("validation_" + matcher[2]);
+      }
+
+      matcher = value.match(this.invalidTestKind);
+      if (matcher != null && matcher.length == 2) {
+        return "Ungültige Testart: " + this.transform(matcher[1]);
+      }
+    }
+    return value;
   }
 
 }
